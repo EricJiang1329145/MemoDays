39f7023af02158577d56fb8ffb4381254155bd46
EricJiang1329145 <jmr_eric@outlook.com>
2025-05-03 15:19:11 +0800

    Initial Commit

---
+++ MemoDays/MemoDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoDaysApp.swift
+//  MemoDays
+//
+//  Created by Eric Jiang on 2025/5/3.
+//
+
+import SwiftUI
+
+@main
+struct MemoDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoDaysTests/MemoDaysTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoDaysTests.swift
+//  MemoDaysTests
+//
+//  Created by Eric Jiang on 2025/5/3.
+//
+
+import Testing
+@testable import MemoDays
+
+struct MemoDaysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
